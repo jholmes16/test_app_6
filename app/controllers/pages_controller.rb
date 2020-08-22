f22e357eeb7da11df730c541232b85542d2e1f14
Jahmaul Holmes <jahmaulschool@gmail.com>
2020-08-22 11:48:55 -0400

    Initial commit and add root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  #method for home.html.erb under views > pages
+  def home
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+#sends to pages controller under controllers folder and home method
+  root 'pages#home'
+end
