2b442951e4ecc9be1ddd8fc1733d5a23973b33e2
Jahmaul Holmes <jahmaulschool@gmail.com>
2020-08-23 11:36:51 -0400

    Add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   #method for home.html.erb under views > pages
   def home
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 #sends to pages controller under controllers folder and home method
   root 'pages#home'
+  get 'about', to: 'pages#about'
 end
